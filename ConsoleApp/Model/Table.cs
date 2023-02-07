e14ae5728c4564b1e99385912bdbf1a9805494aa
Maciej Bialorucki <maciej.bialorucki@gmail.com>
2023-02-07 22:12:43 +0100

    Dodaje metody dostępowe do pola klasy modelu

--- ConsoleApp/Model/Table.cs
+++ ConsoleApp/Model/Table.cs
@@ -4,16 +4,17 @@ namespace ConsoleApp.Model
 {
     public class Table : DatabaseObjectBase
     {
-        public IList<Column> Columns;
 
         public Table()
         {
             Columns = new List<Column>();
         }
-        public string Schema;
+        public string Schema { get; set; }
         public string ParentName { get; set; }
         public string ParentType { get; set; }
         public double NumberOfChildren { get => Columns.Count; }
+
+        public IList<Column> Columns;
 
 
     }
